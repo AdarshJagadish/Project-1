33d4fc0a02be1b425998211dc55fd96d649aa59a
Adarsh Jagadish <abineshrl476@gmail.com>
2024-09-27 17:56:24 +0530

    initial commit

---
+++ Python/List/Slicing.py
@@ -0,0 +1,5 @@
+s="welcome"
+print(s[0])
+print(s[:])       #slicing- starting value : ending value
+print(s[2:6:1])       #slicing- starting value : ending value : incriment/decriment
+print(s[::-1])       #to print the reverse of a string
--- Python/List/negletDuplicate.py
+++ Python/List/negletDuplicate.py
@@ -1,6 +1,14 @@
 l=[2,5,6,4,9,1,5,3,5,4,7,5,6,3,2,1]
+l.sort()
 print(l)
+# for i in l:
+#     if l.count(i)>=2:
+#         l.remove(i)    
+# print(l)
+
+
+l1=[]
 for i in l:
-    if l.count(i)>=2:
-        l.pop(i)    
-print(l)
+    if i not in l1:
+        l1.append(i)
+print(l1)
