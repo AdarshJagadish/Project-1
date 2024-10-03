3690350be59e5bea92c8f47823a7a8403241d5b1
Adarsh Jagadish <abineshrl476@gmail.com>
2024-10-03 17:32:59 +0530

    initial commit

---
+++ Python/pgms/LibrarySystem.py
@@ -0,0 +1,91 @@
+user=[{'id': 100, 'name': 'anu', 'email': 'anu@gmail.com', 'phno': '954258625', 'password': 'anu'}, {'id': 100, 'name': 'amal', 'email': 'amal@gmail.com', 'phno': '964582262', 'password': 'amal'}]
+book=[{'book_id': 100, 'book_name': 'wings of fire', 'stock': 4, 'price': 549}, {'book_id': 101, 'book_name': 'mambazham', 'stock': 2, 'price': 249}]
+while True:
+    print('''
+    1.Register
+    2.Login
+    3.Exit
+    ''')
+    ch=int(input('enter choice'))
+    if ch==1:
+        if len(user)==0:
+            id=100
+        else:
+            id=user[-1]['id']+1
+        # print(id)
+        name=input('enter name')
+        email=input('enter email')
+        phno=input('enter phno')
+        password=input('enter password')
+        user.append({'id':id,'name':name,'email':email,'phno':phno,'password':password})
+        # print(user)
+    elif ch==2:
+        print(user)
+        uname=input('enter uname')
+        password=input('enter password')
+        f=0
+        if uname=='admin' and password=='admin':
+            print('admin login successfull')
+            f=1
+            while True:
+                print('''
+            1.add book
+            2.update
+            3.delete
+            4.view book
+            5.view user
+            6.logout
+            ''')
+                sub_ch=int(input('enter ur choice '))
+                if sub_ch==1:
+                    if len(book)==0:
+                        book_id=100
+                    else:
+                        book_id=book[-1]['book_id']+1
+                        # print(id)
+                        book_name=input('enter Book Name')
+                        stock=int(input('enter available Stock'))
+                        price=int(input('enter Price'))
+                        book.append({'book_id':book_id,'book_name':book_name,'stock':stock,'price':price})
+                        # print(book)
+                elif sub_ch==2:
+                    id=int(input('enter id'))
+                    f1=0
+                    for i in book:
+                        if i['id']==id:
+                            f1=1
+                            i['name']=input('enter new name')
+                    if f1==0:
+                        print('book not available')
+                elif sub_ch==3:
+                    id=int(input('enter id'))
+                    f1=0
+                    for i in book:
+                        if i['id']==id:
+                            f1=1
+                            book.remove(i)
+                    if f1==0:
+                        print('book not available')
+                elif sub_ch==4:
+                    print('{:<8}{:<25}{:<10}{:<10}'.format('Book ID','Book Name','Stock','Price'))
+                    print('-'*0)
+                    for i in book:
+                        print('{:<8}{:<25}{:<10}{:<10}'.format(i['book_id'],i['book_name'],i['stock'],i['price']))
+                elif sub_ch==5:
+                    print(user)
+                elif sub_ch==6:
+                    break
+                else:
+                    print('invalid choice')                          
+        if uname.isdigit():
+            uname=int(uname)
+        for i in user:
+            if i['id']==uname and i['password']==password:
+                print('user login successfull')
+                f=2
+        if f==0:
+            print('invalid uname or password')
+    elif ch==3:
+        break
+    else:
+        print('invalid choice')
--- Python/pgms/studentManagmentSystem.py
+++ Python/pgms/studentManagmentSystem.py
@@ -15,17 +15,47 @@ while True:
         course=str(input('course '))
         l.append({'roll_no':roll_no,'name':name,'email':email,'course':course,})
     elif ch==2:
-        print('{:<8}{:<12}{:<20}{:<10}'.format('roll no','Name','Name','Email','course'))
+        print('{:<8}{:<12}{:<20}{:<10}'.format('roll no','name','email','course'))
         print('-'*50)
         for i in l:
             print('{:<8}{:<12}{:<20}{:<10}'.format(i['roll_no'],i['name'],i['email'],i['course']))
-    # elif ch==3:
-        
-    # elif ch==4:
-    #     a=int(input('no1: '))
-    #     b=int(input('no2: '))
-    #     print(a/b)
-    # elif ch==5:
-    #     break
+    elif ch==3:
+        roll_no=int(input('roll_no'))
+        f=0
+        for i in l:
+            if i['roll_no']==roll_no:
+                print('''
+1. Name
+2.Email
+3.Course
+                    ''')
+                o=int(input())
+                if o==1:
+                    name=str(input('new name '))
+                    i['name']=name
+                    f=1
+                elif o==2:
+                    email=str(input('new email '))
+                    i['email']=email
+                    f=1
+                elif o==3:
+                    course=str(input('Course '))
+                    i['course']=course
+                    f=1
+                else:
+                    break
+        if f==0:
+            print('id not available')
+    elif ch==4:
+        roll_no=int(input("enter roll_no"))
+        f=0
+        for i in l:
+            if i['roll_no']==roll_no:
+                l.remove(i)
+                f=1
+        if f==0:
+            print('not a valid roll_no')
+    elif ch==5:
+        break
     else:
         print('Invalid choice')
